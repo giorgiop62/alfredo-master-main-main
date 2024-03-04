abc75cf21e8ce30fd15a2df0f87928ad7385dee3
Giorgio Perez <giorgio.perez@recrea.srl>
2024-03-04 18:11:33 +0100

    img

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -7,82 +7,40 @@ export default {
 <template>
     <main>
         <div class="container-fluid">
-            <h1 class="text-center p-3"> </h1>
-            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="../../js/assets/me.jpg" class="d-block w-100" alt="...">
-
-                    </div>
-                    <div class="carousel-item">
-                        <img src="../../js/assets/Pumiliani-49.JPG" class=" grey d-block w-100" alt="...">
-
-                    </div>
-                    <div class="carousel-item">
-                        <img src="../assets/Pumiliani-33.JPG" class="grey d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="../assets/notte.jpg" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+            <img src="../assets/violinome.jpg" alt="">
+            <div class="title mt-5">
+                <h1 class="text"> Violinista Compositore performer </h1>
             </div>
         </div>
     </main>
 </template>
-  
+
 <style lang="scss" scoped>
 .container-fluid {
     background-color: black;
     color: white;
-    height: 100vh;
+    display: flex;
 }
+
+
+h1 {
+    text-align: center;
+    font-size: 50px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
 
 .grey {
     filter: grayscale(100%);
 }
 
 img {
-    width: 100%;
-    max-height: 700px;
-    object-fit: cover;
+    height: 100vh;
 }
 
-.carousel {
-    width: 70%;
-    margin: 0 auto;
-    margin-top: 30px;
-    margin-bottom: 30px;
-}
+
 
 a {
     color: white;
 }
-
-.gruppo {
-    max-width: 100%;
-    height: auto;
-}
-
-.sub-img {
-    padding: 20px;
-    margin: 0 auto;
-    max-width: 600px;
-}
-
-@media (max-width: 767px) {
-    .carousel {
-        width: 100%;
-    }
-}
 </style>
-  
